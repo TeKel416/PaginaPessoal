2bbacd73b1dc7ca8718f3ff1fc7d8b0688d414cd
Maria Eduarda de S. Barros <84398771+TeKel416@users.noreply.github.com>
2021-08-18 20:50:12 -0300

    Add files via upload

--- tamanhotitulo.js
+++ tamanhotitulo.js
@@ -1,3 +1,14 @@
+document.addEventListener("DOMContentLoaded", inserirBotoesComInnerHTML);
+
+function inserirBotoesComInnerHTML(){
+    div = document.createElement("div");
+    div.innerHTML =`
+    <button onclick="mudartamanho('70')">+</button>
+    <button onclick="mudartamanho('20')">-</button>
+    `
+    document.body.prepend(div);
+}
+
 function mudartamanho(tamanho){
     h2 = document.querySelector('h1');
     h2.style.fontSize = tamanho+'px';
